48fd2b14c830362ddca920188309a9aed08d5f9d
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-09-29 18:06:34 +0000

    feat: change to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,9 +12,12 @@ export const environment = {
   production: true,
   lensConfig: new LensConfig(
     [
-      new Beam('broker', new URL('http://localhost:8085'), [
-        'proxy1',
-        'proxy2',
+      new Beam('broker', new URL('https://explorer.eucaim.cancerimage.eu/backend/'), [
+      // new Beam('broker', new URL('http://localhost:8085'), [
+        // 'proxy1',
+        // 'proxy2',
+	    'chaimeleon',
+	    'procanceri'
       ]),
     ],
     [
